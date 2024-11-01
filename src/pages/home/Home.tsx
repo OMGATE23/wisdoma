5c09af1573ae6171353324b4cca04d62fb9e073b
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2024-11-01 14:23:02 +0530

    feat: homepage demo

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -18,23 +18,34 @@ export default function Home() {
           Login
         </Link>
       </div>
-      <div className="flex items-center px-12 gap-8 justify-center">
-        <div className="min-h-[60vh] w-[80%] md:w-[50%] flex flex-col justify-center items-center text-center">
-          <h1 className="font-lora text-6xl font-[600]">Wisdoma</h1>
-          <h2 className="font-lora text-xl font-[600] text-neutral-700">
-            The best note taking app of{' '}
-            <span className="text-neutral-900 font-lora">21st century</span>
-          </h2>
-          <h3 className="text-neutral-500 font-lora mt-4">
-            Transform your thoughts into organized masterpieces. Experience
-            note-taking reimagined for the modern age.
-          </h3>
-          <Link
-            to="/signup"
-            className="flex items-center gap-2 hover:gap-4 transition-all duration-100 py-2 px-6 rounded bg-neutral-900 text-white w-fit text-sm mt-4 hover:bg-black"
-          >
-            Get Started <ArrowRightIcon />
-          </Link>
+      <div className='flex justify-center px-4 flex-col items-center my-12 gap-16'>
+        <div className="flex items-center px-12 justify-center">
+          <div className=" w-[80%] md:w-[60%] flex flex-col justify-center items-center text-center">
+            <h1 className="font-lora text-6xl font-[600]">Wisdoma</h1>
+            <h2 className="font-lora text-xl font-[600] text-neutral-700">
+            Visualizing Your Notes through{' '}
+              <span className="text-black font-lora">Knowledge Graphs</span>
+            </h2>
+            <h3 className="text-neutral-500 font-lora mt-4">
+              Transform your thoughts into organized masterpieces. Experience
+              note-taking reimagined for the modern age.
+            </h3>
+            <Link
+              to="/signup"
+              className="flex items-center gap-2 hover:gap-4 transition-all duration-100 py-2 px-6 rounded bg-neutral-900 text-white w-fit text-sm mt-4 hover:bg-black"
+            >
+              Get Started <ArrowRightIcon />
+            </Link>
+          </div>
+        </div>
+        <div className='w-full md:w-[80%] aspect-video flex flex-col gap-4'>
+          <h3 className='text-2xl font-[600] text-center'>Checkout our interactive demo!</h3>
+        <iframe 
+          style={{border: "none"}} 
+          width="100%" 
+          height="100%" 
+          src="https://app.staging.sharefable.com/embed/demo/dnu-om-wisdoma-cj8wzwe500f6kile" 
+          allowFullScreen></iframe>
         </div>
       </div>
 
@@ -44,7 +55,7 @@ export default function Home() {
             Store your notes in an intuitive folder structure
           </h2>
           <img
-            className="block w-[75%] md:w-[560px] outline outline-2 outline-neutral-200 rounded-md shadow-md"
+            className="block w-[75%] md:w-[720px] rounded-md shadow"
             alt="folder image"
             src="/folder.png"
           />
@@ -54,7 +65,7 @@ export default function Home() {
             Write stellar notes with our awesome editor
           </h2>
           <img
-            className="block w-[75%] md:w-[560px] outline outline-2 outline-neutral-200 rounded-md shadow-md"
+            className="block w-[75%] md:w-[720px] rounded-md shadow"
             alt="folder image"
             src="/note.png"
           />
@@ -65,7 +76,7 @@ export default function Home() {
             Visualize your notes like never before
           </h2>
           <img
-            className="block w-[75%] md:w-[560px] outline outline-2 outline-neutral-200 rounded-md shadow-md"
+            className="block w-[75%] md:w-[720px] rounded-md shadow"
             alt="folder image"
             src="/graphview.png"
           />
